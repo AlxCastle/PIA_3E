c0ebf0d96ac9523f494f2440151d1a8988ef82fc
3milion <167380947+3milion@users.noreply.github.com>
2024-10-19 18:26:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import argparse
 #APIKEY suggested: skoTKeGUubhAIZbKPZEBpEeEiuk8o5Wu
 parser = argparse.ArgumentParser(description="Search IP", \
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument("-API_KEY", dest="API_KEY", \
+parser.add_argument("-API_KEY_sho", dest="API_KEY_sho", \
                     help="API KEY required to use the Shodan API", required=True)
 parser.add_argument("-ports", dest="ports", \
                     help="El puerto que quieres buscar que este abierto", default="80")
 params=parser.parse_args()
 
 ports_shodan="port: "+params.ports
-APIKEY=params.API_KEY
+APIKEY_shodan=params.API_KEY_sho
