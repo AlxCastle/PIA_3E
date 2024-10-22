ffdd093bf7bf3002dbd380705980c9d72a7d224f
3milion <167380947+3milion@users.noreply.github.com>
2024-10-21 21:45:36 -0600

    Update modules_api.py

--- Modules/modules_api.py
+++ Modules/modules_api.py
@@ -49,7 +49,7 @@ def search_vulnerabilities(APIKEY,Port_shodan):
 
                     if match["ip_str"] is not None:
                         print("IP:", match['ip_str'])
-                        print("Puerto:", match['port'])
+                        print("Puerto(s):", match['port'])
 
                         if 'vulns' in match:
 
@@ -243,79 +243,133 @@ def suspicious_ip(APIKEY):
     
         #It connects to the API with the Blacklist function and if it cannot be done, an error message will be sent.
 
-    except Exception as e:
-        logging.error("Hubo error con la APIKEY ya que no contesto la API")
-        print("Hubo un error al conectarse con la API", e)
-        exit()
-    
-    else:
+    def Suspicious_IP(APIKEY):
+        logging.basicConfig(filename='module_IPAbuseDB.log', level=logging.INFO)
 
-        logging.info("Se guarda la informacion que pposteriormente se mostrara en pantalla o guardara")
-        print("Ingrese [1]-Para ver los resultados en pantalla\n[2]-Para generar un reporte de texto")
-        op=int(input())
+        logging.info("Primero se obtiene en variables la url y todo lo que se ocupa para conectarse a la API")
+        url_blacklist = 'https://api.abuseipdb.com/api/v2/blacklist'
+        
+        #The url variables and parameters are being saved to connect with the Blacklist function api
 
-        while op!=1 and op!=2:
-                
-        #A loop is created until get a correct option.
+        querystring_blacklist={
+            'confidenceMinimum':'90'
+        }
 
-            print("Elija una opcion valida")
-            print("Presione [1]-Para ver en pantalla los resultados\n[2]-Para generar un reporte de texto")
+        #The headers are saved with the APIKEY
+
+        headers_blacklist={
+            'Accept': 'application/json',
+            'Key': APIKEY
+        }
+
+        #The url and header variables of the API check function are saved
+
+        url_check='https://api.abuseipdb.com/api/v2/check'
+
+        headers_check={
+            'Accept': 'application/json',
+            'Key': APIKEY
+        }
+
+        try:
+            logging.info("Se conecta con la API en cuanto a la Blacklist")
+            r_blacklist=requests.get(url_blacklist, headers=headers_blacklist, params=querystring_blacklist)
+            r_json_blacklist=r_blacklist.json()
+            len_blacklist=r_json_blacklist["data"]
+        
+            #It connects to the API with the Blacklist function and if it cannot be done, an error message will be sent.
+
+        except Exception as e:
+            logging.error("Hubo error con la APIKEY ya que no contesto la API")
+            print("Hubo un error al conectarse con la API", e)
+            exit()
+        
+        else:
+
+            logging.info("Se guarda la informacion que pposteriormente se mostrara en pantalla o guardara")
+            print("Ingrese [1]-Para ver los resultados en pantalla\n[2]-Para generar un reporte de texto")
             op=int(input())
 
-        logging.info("Con un ciclo for se obtienen todas las ips sospechosas")
-        for i in range(len(len_blacklist)):
-            querystring_check={
-            'ipAddress': r_json_blacklist["data"][i]["ipAddress"],
-            'maxAgeInDays': '90'
-    
-            #Suspicious IPs are being checked
+            while op!=1 and op!=2:
+                    
+            #A loop is created until get a correct option.
 
-            }
-            try:
-                logging.info("Se conecta con la API en cuanto a la Blacklist")
-                r_check=requests.get(url_check, headers=headers_check, params=querystring_check)
-                r_json_check=r_check.json()
-    
-                #connects with the check function of the API
+                print("Elija una opcion valida")
+                print("Presione [1]-Para ver en pantalla los resultados\n[2]-Para generar un reporte de texto")
+                op=int(input())
 
-            except Exception as e:
-                logging.error("Hubo error con la APIKEY ya que no contesto la API")
-                print("Hubo un error con la al conectarse a la api desde la ip",\
-                    r_json_blacklist["data"][i]["ipAddress"], "\n", e)
-                
-            else:
+            logging.info("Con un ciclo for se obtienen todas las ips sospechosas")
+            for i in range(len(len_blacklist)):
+                querystring_check={
+                'ipAddress': r_json_blacklist["data"][i]["ipAddress"],
+                'maxAgeInDays': '90'
+        
+                #Suspicious IPs are being checked
 
-                if op==1:
-                    print("El ip sospechoso es:", r_json_blacklist["data"][i]["ipAddress"])
-                    if "reports" in r_json_check["data"] and len(r_json_check["data"]["reports"]) > 0:
-                        print("Se reportó esta IP por:", r_json_check["data"]["reports"][0]["comment"])
-                        print("La IP sospechosa es del país:", r_json_check["data"]["reports"][0]["reporterCountryName"])
-                    else:
-                        print("No se encontraron reportes recientes para esta IP.")
-                    print("-" * 50)
+                }
+                try:
+                    logging.info("Se conecta con la API en cuanto a la Blacklist")
+                    r_check=requests.get(url_check, headers=headers_check, params=querystring_check)
+                    r_json_check=r_check.json()
+        
+                    #connects with the check function of the API
 
-                    #the results of the suspicious IPs are printed on the screen
-
+                except Exception as e:
+                    logging.error("Hubo error con la APIKEY ya que no contesto la API")
+                    print("Hubo un error con la al conectarse a la api desde la ip",\
+                        r_json_blacklist["data"][i]["ipAddress"], "\n", e)
+                    
                 else:
-                    file_name=input("Ingrese el nombre del reporte junto con la extension \".txt\"")
-                
-                    try:
-                        logging.info("Se intenta crear el archivo")
-                        with open(file_name,"w") as file:
-                            file.write("El ip sospechoso es:"+str(r_json_blacklist["data"][i]["ipAddress"]))
-                            if "reports" in r_json_check["data"] and len(r_json_check["data"]["reports"]) > 0:
-                                file.write("Se reporto esta ip por:"+str(r_json_check["data"]["reports"][0]["comment"]))
-                                file.write("La ip sospechosa es del pais:"+\
-                                        str(r_json_check["data"]["reports"][0]["reporterCountryName"]))
-                            else:
-                                file.write("No se encontraron reportes recientes para esta IP.")
-                            file.write("-"*50)
-                
-                        #The output of the suspicious IP is saved in a file
 
-                    except Exception as e:
-                        logging.error("Hubo un error al crear el archivo")
-                        print("Hubo un error al crear el archivo", e)
-            
-            finally:
-                print("Se termino de ejecutar la funcion") 
+                    if op==1:
+                        print("El ip sospechoso es:", r_json_blacklist["data"][i]["ipAddress"])
+                        if "totalReports" in r_json_check["data"]:
+                            print("Se reportó esta IP por:", r_json_check["data"]["totalReports"])
+                            print("La IP sospechosa es del país(codigo):", r_json_check["data"]["countryCode"])
+                            print("La puntuacion que tiene al abuso de confianza es:",\
+                                str(r_json_check["data"]["abuseConfidenceScore"]), "\n")
+                        else:
+                            print("No se encontraron reportes recientes para esta IP.\n")
+                        print("-" * 50)
+
+                        #the results of the suspicious IPs are printed on the screen
+
+                    else:
+                        if i==0:
+                            file_name=input("Ingrese el nombre del reporte junto con la extension \".txt\"")
+                            with open(file_name, "w") as file:
+                                file.write("El ip sospechoso es:"+str(r_json_blacklist["data"][i]["ipAddress"]))
+                                if "totalReports" in r_json_check["data"]:
+                                    file.write("Se reportó esta IP por:"\
+                                            +str(r_json_check["data"]["totalReports"]))
+                                    file.write("La IP sospechosa es del país(codigo):"\
+                                            +str(r_json_check["data"]["countryCode"]))
+                                    file.write("La puntuacion que tiene al abuso de confianza es: "\
+                                        +str(r_json_check["data"]["abuseConfidenceScore"])+"\n")
+                                else:
+                                    file.write("No se encontraron reportes recientes para esta IP.\n")
+                                file.write("-"*50)                            
+
+                        try:
+                            logging.info("Se intenta crear el archivo")
+                            file=open(file_name, "a")
+                            file.write("El ip sospechoso es:"+str(r_json_blacklist["data"][i]["ipAddress"]))
+                            if "totalReports" in r_json_check["data"] :
+                                file.write("Se reportó esta IP por:"\
+                                        +str(r_json_check["data"]["totalReports"]))
+                                file.write("La IP sospechosa es del país(codigo):"\
+                                        +str(r_json_check["data"]["countryCode"]))
+                                file.write("La puntuacion que tiene al abuso de confianza es: "\
+                                    +str(r_json_check["data"]["abuseConfidenceScore"])+"\n")
+                            else:
+                                file.write("No se encontraron reportes recientes para esta IP.\n")
+                            file.write("-"*50+"\n")
+                            file.close()
+                    
+                            #The output of the suspicious IP is saved in a file
+
+                        except Exception as e:
+                            logging.error("Hubo un error al crear el archivo")
+                            print("Hubo un error al crear el archivo", e)  
+        finally:
+            print("Se termino de ejecutar la funcion")
